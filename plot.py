7c10a44f97baabbaf543434252ede2f17dfdf8e7
rigidlab <phuta@rigidlab.com>
2020-12-24 10:20:56 -0800

    Use api to download data directly

--- plot.py
+++ plot.py
@@ -1,32 +1,29 @@
-import argparse
+import requests
 import pandas as pd
 import altair as alt
 from altair import datum
 from bokeh.plotting import figure,show,save
 
 def main():
-    parser=argparse.ArgumentParser(description='Plot COVID-19 Data')
-    parser.add_argument('--cases',help='Cases CSV file',
-        default='COVID-19_case_counts_by_date.csv')
-    parser.add_argument('--hospitalizations',help='Hospitalizations CSV file',
-        default='COVID-19_hospitalizations_by_date.csv')
-    args=parser.parse_args()
-
-    cases_df=pd.read_csv(args.cases)
-    hos_df=pd.read_csv(args.hospitalizations)
-    df=pd.merge(cases_df,hos_df,on='Date')
-    df=df.melt(id_vars=['Date'],var_name='Type',value_name='Count')
+    cases_json=requests.get('https://data.sccgov.org/resource/6cnm-gchg.json').json()
+    hos_json=requests.get('https://data.sccgov.org/resource/5xkz-6esm.json').json()
+    cases_df=pd.DataFrame(cases_json)
+    hos_df=pd.DataFrame(hos_json)
+    print(cases_df.head())
+    print(hos_df.head())
+    df=pd.merge(cases_df,hos_df,on='date')
+    df=df.melt(id_vars=['date'],var_name='Type',value_name='Count')
     scales = alt.selection_interval(bind='scales',encodings=['x'])
     brush = alt.selection(type='interval', encodings=['x'])
 
     base=alt.Chart(df).encode(
-        alt.X('Date:T',axis=alt.Axis(title=None)),
+        alt.X('date:T',axis=alt.Axis(title=None)),
     )
     bar=base.mark_bar(color='purple').encode(
         alt.Y('Count:Q',axis=alt.Axis(title='New Cases')),
         tooltip='Count:Q',
     ).transform_filter(
-        datum.Type=='New_cases'
+        datum.Type=='new_cases'
     )
 
     bar2=base.mark_bar().encode(
@@ -40,7 +37,7 @@ def main():
             axis=alt.Axis(title='Total cases')
         )
     ).transform_filter(
-        datum.Type=='Total_cases'
+        datum.Type=='total_cases'
     )
     #chart=(bar+bar2).add_selection(scales)
     cases=alt.layer(bar,line).resolve_scale(
@@ -61,7 +58,7 @@ def main():
     )
 
     chart=cases&hos
-    chart.save('covid-19.html')
+    chart.save('index.html')
 
 if __name__=='__main__':
     main()
