d73697a7521e62aec83a358c76996f59e6936a36
rigidlab <phuta@rigidlab.com>
2022-01-04 20:49:02 -0800

    Remove hospitalization

--- plot.py
+++ plot.py
@@ -6,13 +6,21 @@ from bokeh.plotting import figure,show,save
 
 def main():
     cases_json=requests.get('https://data.sccgov.org/resource/6cnm-gchg.json').json()
-    hos_json=requests.get('https://data.sccgov.org/resource/5xkz-6esm.json').json()
+    #hos_json=requests.get('https://data.sccgov.org/resource/5xkz-6esm.json').json()
     cases_df=pd.DataFrame(cases_json)
+<<<<<<< HEAD
+    #hos_df=pd.DataFrame(hos_json)
+    #df=pd.merge(cases_df,hos_df,on='date')
+    #df=df.melt(id_vars=['date'],var_name='Type',value_name='Count')
+    df=cases_df
+    print(df)
+=======
     hos_df=pd.DataFrame(hos_json)
     print(cases_df.head())
     print(hos_df.head())
     df=pd.merge(cases_df,hos_df,how='left',on='date')
     df=df.melt(id_vars=['date'],var_name='Type',value_name='Count')
+>>>>>>> bd66762f5a11334319b4427462b2805e814eb47d
     scales = alt.selection_interval(bind='scales',encodings=['x'])
     brush = alt.selection(type='interval', encodings=['x'])
 
@@ -20,26 +28,15 @@ def main():
         alt.X('date:T',axis=alt.Axis(title=None)),
     )
     bar=base.mark_bar(color='purple').encode(
-        alt.Y('Count:Q',axis=alt.Axis(title='New Cases')),
-        tooltip='Count:Q',
-    ).transform_filter(
-        datum.Type=='new_cases'
-    )
-
-    bar2=base.mark_bar().encode(
-        alt.Y('Count:Q',axis=alt.Axis(title='Hospitalizations')),
-        color='Type'
-    ).transform_filter(
-        (datum.Type=='non_icu_covid')  | (datum.Type=='icu_covid')
+        alt.Y('new_cases:Q',axis=alt.Axis(title='New Cases')),
+        tooltip='new_cases:Q',
     )
     line=base.mark_line(color='red').encode(
-        alt.Y('Count:Q',
+        alt.Y('total_cases:Q',
             axis=alt.Axis(title='Total cases')
-        )
-    ).transform_filter(
-        datum.Type=='total_cases'
+        ),
+        tooltip='total_cases:Q'
     )
-    #chart=(bar+bar2).add_selection(scales)
     cases=alt.layer(bar,line).resolve_scale(
         y='independent'
     ).properties(
@@ -47,17 +44,9 @@ def main():
         height=300,
         title='Santa Clara'
     ).add_selection(
-        scales,
+        scales
     )
-    hos=bar2.properties(
-        width=700,
-        height=300,
-        title='Santa Clara'
-    ).add_selection(
-        scales,
-    )
-
-    chart=cases&hos
+    chart=cases
     chart.save('index.html')
 
 if __name__=='__main__':
